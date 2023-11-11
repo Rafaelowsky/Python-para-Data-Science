e3f4b324a5d5b0c47623206cbcc5b396c029aba3
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-10 22:02:17 -0600

    Añadí código para imprimir los primeros 10 registros

--- Archivos/primer_ejercicio.py
+++ Archivos/primer_ejercicio.py
@@ -3,4 +3,7 @@ pd.set_option('display.max_rows', 10)
 pd.set_option('display.max_columns', 10)
 
 dataset = pd.read_csv('C:\\Users\\rzava\\OneDrive\\Documentos\\DataSciencePython\\Archivos\\db.csv', sep= ';')
-print(dataset)
+
+# 1. Mostrar los primeros 10 registros
+
+print(dataset.head(10))
