6c9aee2a2339f23a0863d1ebf9ade20612aebf4c
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-10 21:52:11 -0600

    Añadí el primer programa y hoja de datos

---
+++ primer_ejercicio.py
@@ -0,0 +1,6 @@
+import pandas as pd
+pd.set_option('display.max_rows', 10)
+pd.set_option('display.max_columns', 10)
+
+dataset = pd.read_csv('C:\\Users\\rzava\\OneDrive\\Documentos\\DataSciencePython\\db.csv', sep= ';')
+print(dataset)
