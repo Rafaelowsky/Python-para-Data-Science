07884fc5d6abd4d98f4a9ec53b9bb718c02046e2
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-10 21:59:45 -0600

    Cambie el nombre de carpeta y corregí la dirección

--- Archivos/primer_ejercicio.py
+++ Archivos/primer_ejercicio.py
@@ -2,5 +2,5 @@ import pandas as pd
 pd.set_option('display.max_rows', 10)
 pd.set_option('display.max_columns', 10)
 
-dataset = pd.read_csv('C:\\Users\\rzava\\OneDrive\\Documentos\\DataSciencePython\\db.csv', sep= ';')
+dataset = pd.read_csv('C:\\Users\\rzava\\OneDrive\\Documentos\\DataSciencePython\\Archivos\\db.csv', sep= ';')
 print(dataset)
