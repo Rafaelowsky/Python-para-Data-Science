5dd9e923854e66a7fa5b93ce131cc93988c610dc
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-10 22:05:22 -0600

    Añadí comentarios de funciones ya existentes

--- Archivos/primer_ejercicio.py
+++ Archivos/primer_ejercicio.py
@@ -1,9 +1,12 @@
+# Este import es para poder usar las funciones de pandas
 import pandas as pd
-pd.set_option('display.max_rows', 10)
-pd.set_option('display.max_columns', 10)
 
+# Estas dos lineas son para que solamente se muestren 10 filas y 10 columnas
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.max_rows', 10)
+
+# Guardamos el archivo en una variable
 dataset = pd.read_csv('C:\\Users\\rzava\\OneDrive\\Documentos\\DataSciencePython\\Archivos\\db.csv', sep= ';')
 
-# 1. Mostrar los primeros 10 registros
-
+# Mostrar los primeros 10 registros
 print(dataset.head(10))
