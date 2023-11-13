5a14784673c15530cc2b115f72705a7c9d4c4040
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-13 17:06:40 -0600

    Añadí método para desempaquetar tuplas

---
+++ Archivos/tercer.ejercicio.py
@@ -0,0 +1,7 @@
+nombres_carros = ("Mazda", "Renault", "Chevrolet", "Susuki", "Kia", "Ferrari", "Lamborghini")
+
+# Para desempaquetar una tupla
+
+carro1, carro2, carro3, carro4, carro5, carro6, carro7 = nombres_carros
+
+print(carro1, carro2, carro3, carro4, carro5, carro6, carro7)
