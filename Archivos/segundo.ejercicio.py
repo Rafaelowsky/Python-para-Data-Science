00452c21908d905978b1f70714d7e668fcef1fe0
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-12 23:30:10 -0600

    Añadí método para accesar a tuplas dentro de tuplas

--- Archivos/segundo.ejercicio.py
+++ Archivos/segundo.ejercicio.py
@@ -6,7 +6,7 @@ tupla2 = 1,2,3,4,5,6,7,8,9,10
 
 tupla3 = tuple([1,2,3,4,5,6,7,8,9,10])
 
-marcas_autos = ("BMW", "Mercedes Benz", "Audi", "Ford", "Mazda")
+marcas_autos = ("BMW", "Mercedes Benz", "Audi", "Ford", "Mazda", ("Nissan", "Renault", "Chevrolet"))
 
 # Podemos acceder a los elementos de una tupla de la misma forma que en las listas
 
@@ -19,3 +19,7 @@ print (marcas_autos[0:3])
 # Si queremos acceder a los elementos 0, 1, 2 y 3 podemos hacerlo de esta forma:
 
 print(marcas_autos[:4])
+
+# Para acceder a los elementos dentro de la tupla dentro de la tupla
+
+print(marcas_autos[5][1])
