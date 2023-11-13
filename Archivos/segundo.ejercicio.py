c3b60f6bf7ae9606b5d92b64430820803ed94e47
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-11-12 23:19:56 -0600

    Añadí métodos para acceder a elementos de tuplas

--- Archivos/segundo.ejercicio.py
+++ Archivos/segundo.ejercicio.py
@@ -5,3 +5,17 @@ tupla1 = (1,2,3,4,5,6,7,8,9,10)
 tupla2 = 1,2,3,4,5,6,7,8,9,10
 
 tupla3 = tuple([1,2,3,4,5,6,7,8,9,10])
+
+marcas_autos = ("BMW", "Mercedes Benz", "Audi", "Ford", "Mazda")
+
+# Podemos acceder a los elementos de una tupla de la misma forma que en las listas
+
+print(marcas_autos[0])
+
+# Recordar que el final no se incluye en el rango
+
+print (marcas_autos[0:3])
+
+# Si queremos acceder a los elementos 0, 1, 2 y 3 podemos hacerlo de esta forma:
+
+print(marcas_autos[:4])
